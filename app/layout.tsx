32e4597477916989fcfd6f14830254dca7b3a1b6
Jayden De Souza <jaydendesouza2005@gmail.com>
2024-12-19 22:57:44 +0530

    font+favicon+some packages setup

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Cloud Space",
+  description:
+    "A cutting-edge cloud storage solution designed for seamless, secure, and efficient file management.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
